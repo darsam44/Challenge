dff20f942f4c8d66d7741130925f6f41ff0a0d6f
darsam44 <darsam44@gmail.com>
2020-11-22 18:50:34 +0200

    new page new challenge

--- app/src/main/java/com/example/challenge/Acvtivities/Main_Page.java
+++ app/src/main/java/com/example/challenge/Acvtivities/Main_Page.java
@@ -2,15 +2,31 @@ package com.example.challenge.Acvtivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.challenge.MainActivity;
 import com.example.challenge.R;
 
-public class Main_Page extends AppCompatActivity {
+public class Main_Page extends AppCompatActivity implements View.OnClickListener {
+    Button Chalege;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__page);
+
+        Chalege = (Button) findViewById(R.id.new_challenge);
+        Chalege.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+    if ( view == Chalege){
+        Intent intent = new Intent(Main_Page.this, new_challenge.class);
+        startActivity(intent);
+    }
     }
 }
---
+++ app/src/main/java/com/example/challenge/Acvtivities/new_challenge.java
@@ -0,0 +1,16 @@
+package com.example.challenge.Acvtivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.challenge.R;
+
+public class new_challenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_challenge);
+    }
+}
