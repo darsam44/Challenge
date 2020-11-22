081caf1ff2626c2c6655c629e836db2f15a64756
darsam44 <darsam44@gmail.com>
2020-11-22 16:27:54 +0200

    make the MainActivity

--- app/src/main/java/com/example/challenge/MainActivity.java
+++ app/src/main/java/com/example/challenge/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.challenge.Acvtivities.Main_Page;
+import com.example.challenge.Acvtivities.Registar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button regi;
+    Button Log;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        regi = (Button) findViewById(R.id.b_registar);
+        Log = (Button) findViewById(R.id.b_login);
+        regi .setOnClickListener(this);
+        Log.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if ( view == regi) {
+            Intent intent = new Intent(MainActivity.this, Registar.class);
+            startActivity(intent);
+        }
+        else if ( view == Log){
+            Intent intent = new Intent(MainActivity.this, Main_Page.class);
+            startActivity(intent);
+        }
     }
 }
