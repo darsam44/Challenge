a0dbec937202eeb77ae397aa94625168f5c2b55c
darsam44 <darsam44@gmail.com>
2020-11-25 18:52:01 +0200

    add photo chooser in profile

--- app/src/main/java/com/example/challenge/Acvtivities/Main_Page.java
+++ app/src/main/java/com/example/challenge/Acvtivities/Main_Page.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.challenge.R;
@@ -15,6 +16,7 @@ public class Main_Page extends AppCompatActivity implements View.OnClickListener
     Button B_Funny;
     Button B_Family;
     Button B_Scary;
+    ImageButton B_Profile1;
      
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,9 @@ public class Main_Page extends AppCompatActivity implements View.OnClickListener
         B_Family.setOnClickListener(this);
         B_Scary = (Button) findViewById(R.id.MoveToScarry);
         B_Scary.setOnClickListener(this);
+        B_Profile1 = findViewById(R.id.B_Profile);
+        B_Profile1.setOnClickListener(this);
+
 
     }
 
@@ -48,6 +53,10 @@ public class Main_Page extends AppCompatActivity implements View.OnClickListener
     }
     else if (view == B_Scary){
         Intent intent = new Intent(Main_Page.this, Scary.class);
+        startActivity(intent);
+    }
+    else if (view == B_Profile1){
+        Intent intent = new Intent(Main_Page.this, Profile.class);
         startActivity(intent);
     }
     }
--- app/src/main/java/com/example/challenge/Acvtivities/Profile.java
+++ app/src/main/java/com/example/challenge/Acvtivities/Profile.java
@@ -1,12 +1,22 @@
 package com.example.challenge.Acvtivities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.challenge.R;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements View.OnClickListener {
     String ID;
     String First_Name;
     String Last_Name;
@@ -18,9 +28,75 @@ public class Profile extends AppCompatActivity {
     int DO;
     int DECLINE;
 
+    ImageView Profile;
+    Button Choose;
+
+    private static final int IMAGE_PICK_MODE = 1000;
+    private static final int PREMISSION_CODE = 1001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Profile = findViewById(R.id.image_profile);
+        Choose = findViewById(R.id.choose_ima);
+        Choose.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == Choose){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    // premission not granted ask for it
+                    String [] premission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    //show pop up for runtime premission
+                    requestPermissions(premission , PREMISSION_CODE);
+                }
+                else{
+                    // premission already granted
+                    PickImageFromGallery();
+                }
+            }
+            else{
+                // system os is less the marshmelo
+                PickImageFromGallery();
+            }
+        }
+
+
+    }
+
+    private void PickImageFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent , IMAGE_PICK_MODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case PREMISSION_CODE:{
+                if ( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    PickImageFromGallery();
+                }
+                else{
+                    //permission was denied
+                    Toast.makeText(this, "Permission denied" , Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    //handle resulet of image pick
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && resultCode == IMAGE_PICK_MODE) {
+            //set image to ImageView
+            Profile.setImageURI(data.getData());
+        }
     }
 }
