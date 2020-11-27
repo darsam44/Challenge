7879ff73e8942524a8bf4bfa189aaea835e8ec83
talbg <talbg9@gmail.com>
2020-11-27 16:19:32 +0200

    registar and connect firebase

--- app/src/main/java/com/example/challenge/Acvtivities/Login.java
+++ app/src/main/java/com/example/challenge/Acvtivities/Login.java
@@ -1,24 +1,38 @@
 package com.example.challenge.Acvtivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.challenge.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     Button regi;
     Button Log;
+    EditText mEmail, mPassword;
+    FirebaseAuth fAuth;
 
+    String Email_log , Password_log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        fAuth = FirebaseAuth.getInstance();
+        mEmail       =  (EditText) findViewById(R.id.EmailLogin);
+        mPassword    =  (EditText)findViewById(R.id.PasswordLogin);
         regi = (Button) findViewById(R.id.b_registar);
         Log = (Button) findViewById(R.id.b_login);
         regi .setOnClickListener(this);
@@ -26,16 +40,56 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     }
 
+    public void Logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext() , Register.class));
+        finish();
+    }
+
 
     @Override
     public void onClick(View view) {
         if ( view == regi) {
-            Intent intent = new Intent(Login.this, Registar.class);
+            Intent intent = new Intent(Login.this, Register.class);
             startActivity(intent);
         }
         else if ( view == Log){
-            Intent intent = new Intent(Login.this, Main_Page.class);
-            startActivity(intent);
+            SingIn();
+//            Intent intent = new Intent(Login.this, Main_Page.class);
+//            startActivity(intent);
         }
+    }
+
+    private void SingIn() {
+
+        Email_log = mEmail.getText().toString().trim();
+        Password_log  = mPassword.getText().toString().trim();
+
+
+        if(TextUtils.isEmpty(Email_log)){
+            mEmail.setError("Email is Required.");
+            return;
+        }
+        if(TextUtils.isEmpty(Password_log)){
+            mPassword.setError("Password is Required.");
+            return;
+        }
+        if(Password_log.length() < 6){
+            mPassword.setError("Password Must be >= 6 Characters");
+            return;
+        }
+
+        fAuth.signInWithEmailAndPassword(Email_log, Password_log).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText( Login.this, "log in succesful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),Main_Page.class)  );
+                }
+                else{
+                    Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT ).show();
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/challenge/Acvtivities/Register.java
@@ -0,0 +1,132 @@
+package com.example.challenge.Acvtivities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.challenge.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    EditText mFirstName,mLastName, mUserName, mEmail,mPassword,mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ImageView home;
+
+    String ID;
+    String First_Name;
+    String Last_Name;
+    String User_Name;
+    String Password;
+    String Email;
+    String Phone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registar);
+
+        mFirstName   =  (EditText) findViewById(R.id.FirstName);
+        mLastName    =  (EditText) findViewById(R.id.LastName);
+        mUserName    =  (EditText) findViewById(R.id.UserName);
+        mEmail       =  (EditText) findViewById(R.id.Email);
+        mPassword    =  (EditText)findViewById(R.id.Password);
+        mPhone       =  (EditText) findViewById(R.id.PhoneNumber);
+        mRegisterBtn =  findViewById(R.id.Register);
+        home = findViewById(R.id.image_home_re);
+        home.setOnClickListener(this);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mRegisterBtn.setOnClickListener(this);
+
+        // check if their is already connected user
+        if(fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(),Login.class)  );
+                finish();
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == home){
+            Intent intent = new Intent( Register.this , Login.class);
+            startActivity(intent);
+        }
+        else if (view == mRegisterBtn ){
+            CheckIfEmpty();
+        }
+    }
+
+    private void CheckIfEmpty() {
+
+        Email = mEmail.getText().toString().trim();
+        Password  = mPassword.getText().toString().trim();
+        First_Name= mFirstName.getText().toString();
+        Last_Name= mLastName.getText().toString();
+        Phone    = mPhone.getText().toString();
+        User_Name = mUserName.getText().toString();
+
+        if(TextUtils.isEmpty(Email)){
+            mEmail.setError("Email is Required.");
+            return;
+        }
+        if(TextUtils.isEmpty(Last_Name)){
+            mLastName.setError("LastName is Required.");
+            return;
+        }
+        if(TextUtils.isEmpty(First_Name)){
+            mFirstName.setError("FirstName is Required.");
+            return;
+        }
+        if(TextUtils.isEmpty(User_Name)){
+            mUserName.setError("UserName is Required.");
+            return;
+        }
+        if(TextUtils.isEmpty(Phone)){
+            mPhone.setError("Phone is Required.");
+            return;
+        }
+
+        if(TextUtils.isEmpty(Password)){
+            mPassword.setError("Password is Required.");
+            return;
+        }
+
+        if(Password.length() < 6){
+            mPassword.setError("Password Must be >= 6 Characters");
+            return;
+        }
+
+        fAuth.createUserWithEmailAndPassword(this.Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText( Register.this, "User created", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),Login.class)  );
+                }
+                else{
+                    Toast.makeText(Register.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT ).show();
+                }
+
+                }
+        });
+
+    }
+}
